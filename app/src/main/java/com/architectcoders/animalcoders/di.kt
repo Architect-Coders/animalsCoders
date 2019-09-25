7aa8a8c78a3ac7f67ed64379f7ff4542c0e3249b
Jorge Lavin Montoro <jorge.lavinmontoro@telefonica.com>
2019-09-25 11:44:44 +0200

    feature(animals): removed qualifier

--- app/src/main/java/com/architectcoders/animalcoders/di.kt
+++ app/src/main/java/com/architectcoders/animalcoders/di.kt
@@ -31,7 +31,6 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.android.viewmodel.dsl.viewModel
 import org.koin.core.context.startKoin
-import org.koin.core.qualifier.named
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
